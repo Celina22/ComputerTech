35092ad316e601e45baf2ddf758a16894ebd27fd
45095 <catalano.celina@gmail.com>
2021-11-03 23:11:25 -0300

    subiendo la base

---
+++ Sources/Base/Base de ComputerTech.sql
@@ -0,0 +1,290 @@
+USE [master]
+GO
+/****** Object:  Database [ComputerTech]    Script Date: 03/11/2021 23:09:23 ******/
+CREATE DATABASE [ComputerTech]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'ComputerTech', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\ComputerTech.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'ComputerTech_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\ComputerTech_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [ComputerTech] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [ComputerTech].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [ComputerTech] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [ComputerTech] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [ComputerTech] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [ComputerTech] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [ComputerTech] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [ComputerTech] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [ComputerTech] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [ComputerTech] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [ComputerTech] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [ComputerTech] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [ComputerTech] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [ComputerTech] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [ComputerTech] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [ComputerTech] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [ComputerTech] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [ComputerTech] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [ComputerTech] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [ComputerTech] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [ComputerTech] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [ComputerTech] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [ComputerTech] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [ComputerTech] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [ComputerTech] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [ComputerTech] SET  MULTI_USER 
+GO
+ALTER DATABASE [ComputerTech] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [ComputerTech] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [ComputerTech] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [ComputerTech] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [ComputerTech] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [ComputerTech] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [ComputerTech] SET QUERY_STORE = OFF
+GO
+USE [ComputerTech]
+GO
+/****** Object:  Table [dbo].[Barrios]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Barrios](
+	[id_barrio] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[borrado] [bit] NULL,
+ CONSTRAINT [PK_Barrios] PRIMARY KEY CLUSTERED 
+(
+	[id_barrio] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Clientes]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Clientes](
+	[id_cliente] [int] IDENTITY(1,1) NOT NULL,
+	[cuit] [varchar](50) NULL,
+	[razon_social] [varchar](50) NULL,
+	[borrado] [bit] NULL,
+	[calle] [varchar](500) NULL,
+	[numero] [varchar](50) NULL,
+	[fecha_alta] [datetime] NULL,
+	[id_barrio] [int] NULL,
+	[id_contacto] [int] NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[id_cliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Contactos]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Contactos](
+	[id_contacto] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[apellido] [varchar](50) NULL,
+	[email] [varchar](50) NULL,
+	[telefono] [varchar](50) NULL,
+	[borrado] [bit] NULL,
+ CONSTRAINT [PK_Contactos] PRIMARY KEY CLUSTERED 
+(
+	[id_contacto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Facturas]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Facturas](
+	[id_factura] [int] IDENTITY(1,1) NOT NULL,
+	[numero_factura] [varchar](50) NOT NULL,
+	[id_cliente] [int] NOT NULL,
+	[fecha] [datetime] NOT NULL,
+	[id_usuario_creador] [int] NOT NULL,
+	[total] [float] NULL,
+	[borrado] [bit] NOT NULL,
+ CONSTRAINT [PK_Factura] PRIMARY KEY CLUSTERED 
+(
+	[id_factura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[FacturasDetalle]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FacturasDetalle](
+	[id_detalle_factura] [int] IDENTITY(1,1) NOT NULL,
+	[id_factura] [int] NULL,
+	[numero_orden] [int] NULL,
+	[id_producto] [int] NULL,
+	[id_proyecto] [int] NULL,
+	[precio] [decimal](18, 0) NULL,
+	[cantidad] [int] NULL,
+	[borrado] [bit] NULL,
+ CONSTRAINT [PK_FacturaDetalles] PRIMARY KEY CLUSTERED 
+(
+	[id_detalle_factura] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Perfiles]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Perfiles](
+	[id_perfil] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NOT NULL,
+	[borrado] [bit] NOT NULL,
+ CONSTRAINT [PK_Perfiles] PRIMARY KEY CLUSTERED 
+(
+	[id_perfil] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Productos]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Productos](
+	[id_producto] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[fecha_alta] [datetime] NULL,
+	[borrado] [bit] NULL,
+ CONSTRAINT [PK_Producto] PRIMARY KEY CLUSTERED 
+(
+	[id_producto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Proyectos]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Proyectos](
+	[id_proyecto] [int] IDENTITY(1,1) NOT NULL,
+	[id_producto] [int] NULL,
+	[descripcion] [varchar](50) NULL,
+	[version] [varchar](50) NULL,
+	[alcance] [varchar](50) NULL,
+	[id_responsable] [int] NULL,
+	[fecha_alta] [datetime] NULL,
+	[borrado] [nchar](10) NULL,
+ CONSTRAINT [PK_Proyectos] PRIMARY KEY CLUSTERED 
+(
+	[id_proyecto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 03/11/2021 23:09:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuarios](
+	[id_usuario] [int] IDENTITY(1,1) NOT NULL,
+	[id_perfil] [int] NOT NULL,
+	[usuario] [varchar](50) NOT NULL,
+	[password] [varchar](10) NOT NULL,
+	[email] [varchar](50) NOT NULL,
+	[estado] [varchar](1) NOT NULL,
+	[borrado] [bit] NULL,
+ CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED 
+(
+	[id_usuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Perfiles] ADD  CONSTRAINT [DF_Perfiles_borrado]  DEFAULT ((0)) FOR [borrado]
+GO
+ALTER TABLE [dbo].[Productos] ADD  CONSTRAINT [DF_Productos_borrado]  DEFAULT ((0)) FOR [borrado]
+GO
+ALTER TABLE [dbo].[Usuarios] ADD  CONSTRAINT [DF_Usuarios_borrado]  DEFAULT ((0)) FOR [borrado]
+GO
+ALTER TABLE [dbo].[Clientes]  WITH CHECK ADD  CONSTRAINT [FK_Clientes_Barrios] FOREIGN KEY([id_barrio])
+REFERENCES [dbo].[Barrios] ([id_barrio])
+GO
+ALTER TABLE [dbo].[Clientes] CHECK CONSTRAINT [FK_Clientes_Barrios]
+GO
+ALTER TABLE [dbo].[Clientes]  WITH CHECK ADD  CONSTRAINT [FK_Clientes_Contactos] FOREIGN KEY([id_contacto])
+REFERENCES [dbo].[Contactos] ([id_contacto])
+GO
+ALTER TABLE [dbo].[Clientes] CHECK CONSTRAINT [FK_Clientes_Contactos]
+GO
+ALTER TABLE [dbo].[Facturas]  WITH CHECK ADD  CONSTRAINT [FK_Facturas_Clientes] FOREIGN KEY([id_cliente])
+REFERENCES [dbo].[Clientes] ([id_cliente])
+GO
+ALTER TABLE [dbo].[Facturas] CHECK CONSTRAINT [FK_Facturas_Clientes]
+GO
+ALTER TABLE [dbo].[Facturas]  WITH CHECK ADD  CONSTRAINT [FK_Facturas_Usuarios] FOREIGN KEY([id_usuario_creador])
+REFERENCES [dbo].[Usuarios] ([id_usuario])
+GO
+ALTER TABLE [dbo].[Facturas] CHECK CONSTRAINT [FK_Facturas_Usuarios]
+GO
+ALTER TABLE [dbo].[Proyectos]  WITH CHECK ADD  CONSTRAINT [FK_Proyectos_Producto] FOREIGN KEY([id_producto])
+REFERENCES [dbo].[Productos] ([id_producto])
+GO
+ALTER TABLE [dbo].[Proyectos] CHECK CONSTRAINT [FK_Proyectos_Producto]
+GO
+ALTER TABLE [dbo].[Proyectos]  WITH CHECK ADD  CONSTRAINT [FK_Proyectos_Responsable] FOREIGN KEY([id_responsable])
+REFERENCES [dbo].[Usuarios] ([id_usuario])
+GO
+ALTER TABLE [dbo].[Proyectos] CHECK CONSTRAINT [FK_Proyectos_Responsable]
+GO
+ALTER TABLE [dbo].[Usuarios]  WITH CHECK ADD  CONSTRAINT [FK_Usuarios_Perfiles] FOREIGN KEY([id_perfil])
+REFERENCES [dbo].[Perfiles] ([id_perfil])
+GO
+ALTER TABLE [dbo].[Usuarios] CHECK CONSTRAINT [FK_Usuarios_Perfiles]
+GO
+USE [master]
+GO
+ALTER DATABASE [ComputerTech] SET  READ_WRITE 
+GO
